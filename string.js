d29f3c947bdeed831c23b7a25d2c3d6787685ecc
Sonali1505 <sonaliwaghmare851@gmail.com>
2024-09-24 10:07:25 +0530

    learning string in js

---
+++ string.js
@@ -0,0 +1,27 @@
+// let str="sonali";
+// console.log(str.length);
+// console.log(str[0]);
+// //template literals or string interpolation
+
+// let obj= {
+//     item : "pen",
+//     price: 5,
+// };
+
+// let output= `the cost of ${obj.item} is ${obj.price} rupees`;
+// console.log(output);
+
+// console.log(str.toUpperCase);
+// console.log(str.toLowerCase);
+
+// console.log(str.trim);
+
+// console.log(str.slice(1,3));
+
+// let str1="Waghmare";
+// console.log(str.concat(str1));
+
+// console.log(str.replace("s","m"));
+
+// console.log(str.charAt(5));
+
