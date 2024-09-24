3f70ec4593e897b53ff3c3fbcc3eeb0c82c61c3e
Sonali1505 <sonaliwaghmare851@gmail.com>
2024-09-24 09:57:37 +0530

    learing loops in js

---
+++ loops.js
@@ -0,0 +1,39 @@
+// for(let i=1;i<=5;i++){
+//     console.log(i);
+// }
+
+// //for of loop used for string and arrays
+// let str="Sonali";
+// for(let val of str){
+//     console.log(val);
+// }
+
+// //for in loop uesd for objects and arrays
+
+// let student = {
+//     name:"Sonali",
+//     age :20,
+//     cgpa:9.2,
+//     isPass:true,
+// };
+
+// for(let i in student){
+//     console.log(i,student[i]);
+// }
+
+// //print the all the even numbers form 1 to 100
+// for(let i=1;i<=100;i++){
+//     if(i%2===0){
+//         console.log(i);
+//     }
+// }
+
+// //create a game where you start without random game no ask the user to keep guessing the numeber until the user enter the corrrect number
+
+// let gameNum=25;
+// let userNum = prompt("Guess the number");
+// while(userNum!=gameNum){
+//     userNum=prompt("you enterd wrong number Guess again");
+
+// }
+// console.log("congratulations,You enterd right number");
--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-console.log("hello");
-let a= 50;
-//let a=58;This is not allowed
-var b= 45;
-var b=85;//this is possible
-const c= 85;
-let f= true;
-let str= "Sonali ";
-let h;
+// console.log("hello");
+// let a= 50;
+// //let a=58;This is not allowed
+// var b= 45;
+// var b=85;//this is possible
+// const c= 85;
+// let f= true;
+// let str= "Sonali ";
+// let h;
 
 
 
-//operators
+// //operators
 
-d= a+b;//- * / % **
-console.log(d);
+// d= a+b;//- * / % **
+// console.log(d);
 
-console.log(a++);//postincrement
-console.log(++a);//preincrement
-console.log(a--);//postdecrement
-console.log(--a);//predecrement
+// console.log(a++);//postincrement
+// console.log(++a);//preincrement
+// console.log(a--);//postdecrement
+// console.log(--a);//predecrement
 
-a=5;
-b="5";
-console.log(a==b);
-console.log(a===b);
+// a=5;
+// b="5";
+// console.log(a==b);
+// console.log(a===b);
 
-alert("Hello");
-let name = prompt("Enter name");
-console.log(name);
+// // alert("Hello");
+// // let name = prompt("Enter name");
+// // console.log(name);
