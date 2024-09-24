5af7d90ae2be61f87cd4722b221116dca37527a9
Sonali1505 <sonaliwaghmare851@gmail.com>
2024-09-24 10:24:48 +0530

    learing arrays in js

---
+++ arrays.js
@@ -0,0 +1,26 @@
+let marks=[85,93,65,75,95,88];
+// console.log(marks.length);
+
+
+// for(let i=0;i<marks.length;i++){
+//     console.log(marks[i]);
+// }
+
+// for(let val in marks){
+//     console.log(val);//it will give the index values
+// }
+
+// for(let item of marks){
+//     console.log(item);
+// }
+
+console.log(marks.push(96));
+console.log(marks.pop());
+
+console.log(marks.toString());
+
+marks.slice(2,5);
+
+console.log(marks.splice(2,1,55));
+console.log(marks);
+
